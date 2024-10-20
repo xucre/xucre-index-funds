47c2e6bcd2320fc4914fceb5980df4d86b88c0f8
Jordan Paul <jordan@getpixeltag.com>
2024-10-20 15:20:49 -0700

    fixing compile next

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  swcMinify: true,
+  swcMinify: false,
   webpack: (config) => {
     config.experiments = { ...config.experiments, topLevelAwait: true };
     config.resolve.fallback = { fs: false, net: false, tls: false };
